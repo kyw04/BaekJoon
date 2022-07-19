1b7254bf72edcffb7fd5a53304edb015227f0197
kyw04 <85551017+kyw04@users.noreply.github.com>
2022-07-19 23:17:16 +0900

    BaekJoon_15649 N과 M (1)

---
+++ BaekJoon_15649 N과 M (1).cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+int visited[10];
+vector<int> v;
+
+void print()
+{
+    for (auto it = v.begin(); it != v.end(); it++)
+        cout << *it << ' ';
+    cout << '\n';
+}
+
+void backtracking(int size)
+{
+    if (size == m)
+    {
+        print();
+        return;
+    }
+    
+    for (int i = 1; i <= n; i++)
+    {
+        if (visited[i] == 0)
+        {
+            visited[i] = 1;
+            v.push_back(i);
+            backtracking(size + 1);
+            v.pop_back();
+            visited[i] = 0;
+        }
+    }
+}
+
+int main() 
+{
+    cin >> n >> m;
+    
+    backtracking(0);
+    
+    return 0;
+}
