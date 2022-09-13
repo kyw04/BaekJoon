b799ef59c6a7a1f7841f0006c9ef57d212aac898
kyw04 <85551017+kyw04@users.noreply.github.com>
2022-09-13 11:29:28 +0900

    BaekJoon_10867 중복 빼고 정렬하기

---
+++ BaekJoon_10867 중복 빼고 정렬하기.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+	set<int> s;
+	int n;
+	cin >> n;
+	
+	for (int i = 0; i < n; i++)
+	{
+		int input;
+		cin >> input;
+		s.insert(input);
+	}
+	
+	for (auto it = s.begin(); it != s.end(); it++)
+		cout << *it << ' ';
+	
+	
+	return 0;
+}
