f735b8d881e4160c04c44ca385067ee6da746884
kyw04 <85551017+kyw04@users.noreply.github.com>
2022-09-13 10:49:15 +0900

    BaekJoon_11651 좌표 정렬하기 2

---
+++ BaekJoon_11651 좌표 정렬하기 2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+struct Pos
+{
+	int x;
+	int y;
+	
+	bool operator < (Pos& pos) const
+	{
+		if (this->y == pos.y)
+			return this->x < pos.x;
+		
+		return this->y < pos.y;
+	}
+};
+
+ostream& operator << (ostream& os, const Pos& pos)
+{
+	os << pos.x << ' ' << pos.y;
+	return os;
+}
+
+int main()
+{
+	vector<Pos> pos;
+	int n;
+	cin >> n;
+	
+	for (int i = 0; i < n; i++)
+	{
+		int x, y;
+		cin >> x >> y;
+		pos.push_back({ x, y });
+	}
+	
+	sort(pos.begin(), pos.end());
+	
+	for (int i = 0; i < n; i++)
+		cout << pos[i] << '\n';
+		
+	return 0;
+}
