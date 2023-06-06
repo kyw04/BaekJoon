57900ae9dc9bba3ac9c19ded402989dfec6d8b6a
kyw04 <85551017+kyw04@users.noreply.github.com>
2023-06-06 21:57:43 +0900

    BaekJoon_1439 뒤집기

---
+++ BaekJoon_1439 뒤집기.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	string str;
+	int one = 0, zero = 0;
+	cin >> str;
+	
+	for (string::iterator iter = str.begin(); iter != str.end(); iter++)
+	{
+		if (*iter == '0')
+		{
+			
+		}
+		else
+		{
+			
+		}
+	}
+	
+	return 0;
+}
